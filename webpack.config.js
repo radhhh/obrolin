f90012a3d322d88e507510ad93abcb34df87e5fe
radhhh <123277320+radhhh@users.noreply.github.com>
2023-08-25 22:42:12 +0700

    Completed webpack refactorization

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development", 
+  entry:{
+      display: "./src/scripts/display.js",
+      query: "./src/scripts/query.js",
+      ai: "./src/scripts/ai.js",
+      main: "./src/scripts/main.js",
+  },
+  devtool: "inline-source-map",
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Obrolin',
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
