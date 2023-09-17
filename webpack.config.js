d1a77afea6d75c9441dea807f045e5cdbfc39e14
radhhh <123277320+radhhh@users.noreply.github.com>
2023-09-17 19:08:12 +0700

    Change to production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development", 
+  mode: "production", 
   entry:{
       display: "./src/scripts/display.js",
       query: "./src/scripts/query.js",
